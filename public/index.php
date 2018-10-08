adf17f15179d3b7bf07ffb971492b8a88012825c
italo <iep4@njit.edu>
2018-10-08 11:36:23 -0400

    ddsds

--- public/index.php
+++ public/index.php
@@ -1,33 +1,69 @@
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <title>Wello World</title>
-    </head>
-    <link rel="stylesheet" type="text/css" href="extention.html">
+<?php
 
-
-<body>
-
-<form name="form1" enctype="multipart/form-data" method="post" action="spreadsheet.php">
-
-
-    <p>
-        <label>Input File</label>
-        <input type="hidden" name="MAX_FILE_SIZE" value="8000000"/>
-        <input type="file" name="file"/>
-    </p>
-    <p>
-        <label>Convert To</label>
-        <select name="targetFormat">
-            <option value="CSV">CSV</option>
-            <option value="XLS">XLS</option>
-            <option value="XLSX">XLSX</option>
-           
-        </select>
-    </p>
-    <input type="submit" name="submit" value="Proceed" />
-</form>
-
-</body>
-</html>
+main::start("contacts.csv");
+class main  {
+    static public function start($filename) {
+        $records = csv::getRecords($filename);
+        $table = html::generateTable($records);
+    }
+}
+class html {
+    public static function generateTable($records) {
+        $count = 0;
+        foreach ($records as $record) {
+            if($count == 0) {
+                $array = $record->returnArray();
+                $fields = array_keys($array);
+                $values = array_values($array);
+                print_r($fields);
+                print_r($values);
+            } else {
+                $array = $record->returnArray();
+                $values = array_values($array);
+                print_r($values);
+            }
+            $count++;
+        }
+    }
+}
+class csv {
+    static public function getRecords($filename) {
+        $file = fopen($filename,"r");
+        $fieldNames = array();
+        $count = 0;
+        while(! feof($file))
+        {
+            $record = fgetcsv($file);
+            if($count == 0) {
+                $fieldNames = $record;
+            } else {
+                $records[] = recordFactory::create($fieldNames, $record);
+            }
+            $count++;
+        }
+        fclose($file);
+        return $records;
+    }
+}
+class record {
+    public function __construct(Array $fieldNames = null, $values = null )
+    {
+        $record = array_combine($fieldNames, $values);
+        foreach ($record as $property => $value) {
+            $this->createProperty($property, $value);
+        }
+    }
+    public function returnArray() {
+        $array = (array) $this;
+        return $array;
+    }
+    public function createProperty($name = 'first', $value = 'keith') {
+        $this->{$name} = $value;
+    }
+}
+class recordFactory {
+    public static function create(Array $fieldNames = null, Array $values = null) {
+        $record = new record($fieldNames, $values);
+        return $record;
+    }
+}
